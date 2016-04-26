4b5d76c910e8086262a34c5fb44f18cbf0b096a5
Hadi Badjian <hadi@badjian.me>
2016-04-27 02:24:07 +1000

    Add initial TableView project structure Cats

---
+++ cats/Cats/Source/CatsTableViewController.swift
@@ -0,0 +1,18 @@
+//  Copyright © 2016 HB. All rights reserved.
+
+class CatsTableViewController: UITableViewController {
+
+  // MARK: - UIViewController LifeCycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    title = "Cats"
+  }
+
+  // MARK: - UITableViewDelegate
+
+  // MARK: - UITableViewDataSource
+}
+
+import UIKit
