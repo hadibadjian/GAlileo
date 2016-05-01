aafc7ab8ef372ccd20628c20d6ddc42d3f99646a
Hadi Badjian <hadi@badjian.me>
2016-05-02 04:01:31 +1000

    Add Reminders application base layout

---
+++ reminders/Reminders/Sources/MainTableViewController.swift
@@ -0,0 +1,14 @@
+//  Copyright © 2016 HB. All rights reserved.
+
+class MainTableViewController: UITableViewController {
+
+  let menuTableViewCellId = "MenuTableViewCell"
+  let addTableViewCellId = "AddReminderTableViewCell"
+  let reminderViewControllerId = "ReminderViewController"
+
+  // Mark: UITableView Delegate & DataSource
+  // TODO: write down minimum UITableView Delegate & DateSource functionalities
+
+}
+
+import UIKit
